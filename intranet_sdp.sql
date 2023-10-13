b183b99d69e5e5e9420d2426096e33092fd35d99
Florent Maury <flo.neo@hotmail.fr>
2023-10-13 15:20:46 +0200

    M.A.J. base de donnée.

--- intranet_sdp.sql
+++ intranet_sdp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 11 oct. 2023 à 15:56
+-- Généré le : ven. 13 oct. 2023 à 15:20
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -76,25 +76,28 @@ CREATE TABLE `user` (
   `job_3_start` date DEFAULT NULL,
   `job_3_end` date DEFAULT NULL,
   `job_3_exp` text NOT NULL,
-  `contract_type` varchar(10) NOT NULL,
+  `contract_type` text NOT NULL,
   `contract_start` date DEFAULT NULL,
   `contract_end` date DEFAULT NULL,
   `contract_level` varchar(10) NOT NULL,
   `contract_coef` varchar(10) NOT NULL,
-  `contract_remuneration` varchar(8) NOT NULL,
+  `contract_remuneration` float NOT NULL,
   `contract_insurance` varchar(10) NOT NULL,
   `contract_insurance_number` varchar(30) NOT NULL,
   `contract_weekly` varchar(10) NOT NULL,
-  `contract_transports` varchar(10) NOT NULL
+  `contract_transports` varchar(10) NOT NULL,
+  `user_absence` float DEFAULT 0,
+  `user_delay` float DEFAULT 0,
+  `user_extra_time` float NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `surname`, `email`, `secret`, `password`, `birth_date`, `sex`, `phone_number`, `birth_city`, `birth_country`, `current_street_number`, `current_city_street`, `current_city`, `current_zip_code`, `current_country`, `id_number`, `social_security_number`, `creation_date`, `profile_picture`, `insurance_card_face`, `insurance_card_back`, `id_card_face`, `id_card_back`, `school_1`, `school_1_start`, `school_1_end`, `school_1_doc`, `school_2`, `school_2_start`, `school_2_end`, `school_2_doc`, `school_3`, `school_3_start`, `school_3_end`, `school_3_doc`, `job_1`, `job_1_start`, `job_1_end`, `job_1_exp`, `job_2`, `job_2_start`, `job_2_end`, `job_2_exp`, `job_3`, `job_3_start`, `job_3_end`, `job_3_exp`, `contract_type`, `contract_start`, `contract_end`, `contract_level`, `contract_coef`, `contract_remuneration`, `contract_insurance`, `contract_insurance_number`, `contract_weekly`, `contract_transports`) VALUES
-(1, 'Florent', 'Maury', 'e@e.ee', 'bfd53533fbd80993fe5a6d80c7fc2675fdb8a6d91695905527', 'zk32a253b835f3ac4fe27137d4a142440e6ba36620c8345', '1994-06-03', 'Homme', '0612121212', 'Londres', 'Royaume_Uni', '18', 'Rue Saint Antoine', 'Paris', '75004', 'France', '599454644', '1949494052560', '2023-09-28 12:52:07', '6523cc2edd76e9.44144468.webp', '6523e114439f09.87868524.png', '6523d16a5612a5.30537818.jpg', '6523d143ef1753.98244907.webp', '6523d14a1d30c4.20544675.jpg', 'Cloud Campus', '2023-09-01', '2025-09-01', '65252d96962d64.06868687.png', 'Believemy', '2021-12-01', '2023-10-25', '6525637970d291.31234013.png', '', NULL, NULL, '', 'SDP', '2023-09-25', '2024-10-01', 'Lorem ipsum.', 'Sea Sheperd', '2020-05-01', '2023-10-23', 'Lorem ipsum.', 'Lorem', '2023-10-13', '2023-10-27', 'Lorem Ipsum.', '', NULL, NULL, '', '', '', '', '', '', ''),
-(17, 'John', 'Doe', 'o@o.oo', '8f80766a1d4bd2685345aa7263ac06f3f614d0761696948543', 'zk3272f3175835e6b65b315b2a00357f9b9d229dae38345', '0000-00-00', '', '', '', '', '', '', '', '', '', '', '', '2023-10-10 14:35:43', '', '', '', '', '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', '', '', '', '', '');
+INSERT INTO `user` (`id`, `name`, `surname`, `email`, `secret`, `password`, `birth_date`, `sex`, `phone_number`, `birth_city`, `birth_country`, `current_street_number`, `current_city_street`, `current_city`, `current_zip_code`, `current_country`, `id_number`, `social_security_number`, `creation_date`, `profile_picture`, `insurance_card_face`, `insurance_card_back`, `id_card_face`, `id_card_back`, `school_1`, `school_1_start`, `school_1_end`, `school_1_doc`, `school_2`, `school_2_start`, `school_2_end`, `school_2_doc`, `school_3`, `school_3_start`, `school_3_end`, `school_3_doc`, `job_1`, `job_1_start`, `job_1_end`, `job_1_exp`, `job_2`, `job_2_start`, `job_2_end`, `job_2_exp`, `job_3`, `job_3_start`, `job_3_end`, `job_3_exp`, `contract_type`, `contract_start`, `contract_end`, `contract_level`, `contract_coef`, `contract_remuneration`, `contract_insurance`, `contract_insurance_number`, `contract_weekly`, `contract_transports`, `user_absence`, `user_delay`, `user_extra_time`) VALUES
+(1, 'Florent', 'Maury', 'e@e.ee', 'bfd53533fbd80993fe5a6d80c7fc2675fdb8a6d91695905527', 'zk32a253b835f3ac4fe27137d4a142440e6ba36620c8345', '1994-06-03', 'Homme', '0612121212', 'Londres', 'Royaume_Uni', '18', 'Rue Saint Antoine', 'Paris', '75004', 'France', '599454644', '1949494052560', '2023-09-28 12:52:07', '6523cc2edd76e9.44144468.webp', '6523e114439f09.87868524.png', '6523d16a5612a5.30537818.jpg', '6523d143ef1753.98244907.webp', '6523d14a1d30c4.20544675.jpg', 'Cloud Campus', '2023-09-01', '2025-09-01', '65252d96962d64.06868687.png', 'Believemy', '2021-12-01', '2023-10-25', '6525637970d291.31234013.png', '', NULL, NULL, '', 'SDP', '2023-09-25', '2024-10-01', 'Lorem ipsum.', 'Sea Sheperd', '2020-05-01', '2023-10-26', 'Lorem ipsum.', 'Lorem', '2023-10-13', '2023-10-27', 'Lorem Ipsum.', 'CDD', NULL, NULL, '', '', 13, '', '', '35', '', 0, 0, 0),
+(20, 'Karim', 'Doe', 'o@o.oo', '52660f6b103c6f63b62134c7a831dec4a828d1511697122766', 'zk3272f3175835e6b65b315b2a00357f9b9d229dae38345', '0000-00-00', '', '', '', '', '', '', '', '', '', '', '', '2023-10-12 14:59:26', '', '', '', '', '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', NULL, NULL, '', '', 0, '', '', '', '', 0, 0, 0);
 
 --
 -- Index pour les tables déchargées
@@ -114,7 +117,15 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+DELIMITER $$
+--
+-- Évènements
+--
+CREATE DEFINER=`root`@`localhost` EVENT `resetUserTimeBank` ON SCHEDULE EVERY 1 SECOND STARTS '2023-10-13 13:16:24' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE user SET user_extra_time = 0, user_delay = 0, user_absence = 0$$
+
+DELIMITER ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
