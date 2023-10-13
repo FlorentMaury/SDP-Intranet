e974dc072e133efb08d25446902a251270fdb75e
Florent Maury <flo.neo@hotmail.fr>
2023-10-13 12:32:24 +0200

    Début du responsive.

--- model/connectionDBModel.php
+++ model/connectionDBModel.php
@@ -1,21 +1,21 @@
 <?php
 
-// Connexion à la base de donnée : "garage_parrot".
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=intranet_sdp;charset=utf8', 'root', '');
-} catch(Exception $e) {
-    die('Erreur : ' .$e->getMessage());
-};
+    // Connexion à la base de donnée : "garage_parrot".
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=intranet_sdp;charset=utf8', 'root', '');
+    } catch(Exception $e) {
+        die('Erreur : ' .$e->getMessage());
+    };
 
-$users = $bdd->query('SELECT * FROM user ORDER BY surname');
+    $users = $bdd->query('SELECT * FROM user ORDER BY surname');
 
-$resetA = $bdd->prepare('UPDATE user SET user_absence = 0 WHERE DAYOFMONTH(NOW()) = ?');
-$resetA->execute([1]);
+    $resetA = $bdd->prepare('UPDATE user SET user_absence = 0 WHERE DAYOFMONTH(NOW()) = ?');
+    $resetA->execute([1]);
 
-$resetB = $bdd->prepare('UPDATE user SET user_delay = 0 WHERE DAYOFMONTH(NOW()) = ?');
-$resetB->execute([1]);
+    $resetB = $bdd->prepare('UPDATE user SET user_delay = 0 WHERE DAYOFMONTH(NOW()) = ?');
+    $resetB->execute([1]);
 
-$resetC = $bdd->prepare('UPDATE user SET user_extra_time = 0 WHERE DAYOFMONTH(NOW()) = ?');
-$resetC->execute([1]);
+    $resetC = $bdd->prepare('UPDATE user SET user_extra_time = 0 WHERE DAYOFMONTH(NOW()) = ?');
+    $resetC->execute([1]);
 
 ?>
