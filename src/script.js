aeba4a91ffd82f38bab159243d75c96c3be043cb
Florent Maury <flo.neo@hotmail.fr>
2023-10-13 11:53:07 +0200

    Remise du JS.

--- src/script.js
+++ src/script.js
@@ -27,12 +27,12 @@ experiences     = document.querySelector('#experiences');
 contract        = document.querySelector('#contract');
 timeBank        = document.querySelector('#timeBank');
 
-// // Affichage par défaut.
-// managerViewGrid.style.display = 'block';
-// generalInfos.style.display    = 'none';
-// experiences.style.display     = 'none';
-// contract.style.display        = 'none';
-// timeBank.style.display        = 'none';
+// Affichage par défaut.
+managerViewGrid.style.display = 'block';
+generalInfos.style.display    = 'none';
+experiences.style.display     = 'none';
+contract.style.display        = 'none';
+timeBank.style.display        = 'none';
 
 // Apparence.
 managerViewGridButton.style.cursor = 'pointer';
@@ -41,76 +41,76 @@ experiencesButton.style.cursor     = 'pointer';
 contractButton.style.cursor        = 'pointer';
 timeBankButton.style.cursor        = 'pointer';
 
-// // Gestion des boutons.
-// managerViewGridButton.addEventListener('click', () => {
-//     managerViewGrid.style.display = 'block';
-//     generalInfos.style.display    = 'none';
-//     experiences.style.display     = 'none';
-//     contract.style.display        = 'none';
-//     timeBank.style.display        = 'none';
+// Gestion des boutons.
+managerViewGridButton.addEventListener('click', () => {
+    managerViewGrid.style.display = 'block';
+    generalInfos.style.display    = 'none';
+    experiences.style.display     = 'none';
+    contract.style.display        = 'none';
+    timeBank.style.display        = 'none';
 
-//     managerViewGridButton.style.backgroundColor = '#f5f5f5';
-//     generalInfosButton.style.backgroundColor    = '#ffffff';
-//     experiencesButton.style.backgroundColor     = '#ffffff';
-//     contractButton.style.backgroundColor        = '#ffffff';
-//     timeBankButton.style.backgroundColor        = '#ffffff';
-// });
+    managerViewGridButton.style.backgroundColor = '#f5f5f5';
+    generalInfosButton.style.backgroundColor    = '#ffffff';
+    experiencesButton.style.backgroundColor     = '#ffffff';
+    contractButton.style.backgroundColor        = '#ffffff';
+    timeBankButton.style.backgroundColor        = '#ffffff';
+});
 
-// generalInfosButton.addEventListener('click', () => {
-//     managerViewGrid.style.display = 'none';
-//     generalInfos.style.display    = 'block';
-//     experiences.style.display     = 'none';
-//     contract.style.display        = 'none';
-//     timeBank.style.display        = 'none';
+generalInfosButton.addEventListener('click', () => {
+    managerViewGrid.style.display = 'none';
+    generalInfos.style.display    = 'block';
+    experiences.style.display     = 'none';
+    contract.style.display        = 'none';
+    timeBank.style.display        = 'none';
 
-//     managerViewGridButton.style.backgroundColor = '#ffffff';
-//     generalInfosButton.style.backgroundColor    = '#f5f5f5';
-//     experiencesButton.style.backgroundColor     = '#ffffff';
-//     contractButton.style.backgroundColor        = '#ffffff';
-//     timeBankButton.style.backgroundColor        = '#ffffff';
-// });
+    managerViewGridButton.style.backgroundColor = '#ffffff';
+    generalInfosButton.style.backgroundColor    = '#f5f5f5';
+    experiencesButton.style.backgroundColor     = '#ffffff';
+    contractButton.style.backgroundColor        = '#ffffff';
+    timeBankButton.style.backgroundColor        = '#ffffff';
+});
 
-// experiencesButton.addEventListener('click', () => {
-//     managerViewGrid.style.display = 'none';
-//     generalInfos.style.display    = 'none';
-//     experiences.style.display     = 'block';
-//     contract.style.display        = 'none';
-//     timeBank.style.display        = 'none';
+experiencesButton.addEventListener('click', () => {
+    managerViewGrid.style.display = 'none';
+    generalInfos.style.display    = 'none';
+    experiences.style.display     = 'block';
+    contract.style.display        = 'none';
+    timeBank.style.display        = 'none';
 
-//     managerViewGridButton.style.backgroundColor = '#ffffff';
-//     generalInfosButton.style.backgroundColor    = '#ffffff';
-//     experiencesButton.style.backgroundColor     = '#f5f5f5';
-//     contractButton.style.backgroundColor        = '#ffffff';
-//     timeBankButton.style.backgroundColor        = '#ffffff';
-// });
+    managerViewGridButton.style.backgroundColor = '#ffffff';
+    generalInfosButton.style.backgroundColor    = '#ffffff';
+    experiencesButton.style.backgroundColor     = '#f5f5f5';
+    contractButton.style.backgroundColor        = '#ffffff';
+    timeBankButton.style.backgroundColor        = '#ffffff';
+});
 
-// contractButton.addEventListener('click', () => {
-//     managerViewGrid.style.display = 'none';
-//     generalInfos.style.display    = 'none';
-//     experiences.style.display     = 'none';
-//     contract.style.display        = 'block';
-//     timeBank.style.display        = 'none';
+contractButton.addEventListener('click', () => {
+    managerViewGrid.style.display = 'none';
+    generalInfos.style.display    = 'none';
+    experiences.style.display     = 'none';
+    contract.style.display        = 'block';
+    timeBank.style.display        = 'none';
 
-//     managerViewGridButton.style.backgroundColor = '#ffffff';
-//     generalInfosButton.style.backgroundColor    = '#ffffff';
-//     experiencesButton.style.backgroundColor     = '#ffffff';
-//     contractButton.style.backgroundColor        = '#f5f5f5';
-//     timeBankButton.style.backgroundColor        = '#ffffff';
-// });
+    managerViewGridButton.style.backgroundColor = '#ffffff';
+    generalInfosButton.style.backgroundColor    = '#ffffff';
+    experiencesButton.style.backgroundColor     = '#ffffff';
+    contractButton.style.backgroundColor        = '#f5f5f5';
+    timeBankButton.style.backgroundColor        = '#ffffff';
+});
 
-// timeBankButton.addEventListener('click', () => {
-//     managerViewGrid.style.display = 'none';
-//     generalInfos.style.display    = 'none';
-//     experiences.style.display     = 'none';
-//     contract.style.display        = 'none';
-//     timeBank.style.display        = 'block';
+timeBankButton.addEventListener('click', () => {
+    managerViewGrid.style.display = 'none';
+    generalInfos.style.display    = 'none';
+    experiences.style.display     = 'none';
+    contract.style.display        = 'none';
+    timeBank.style.display        = 'block';
 
-//     managerViewGridButton.style.backgroundColor = '#ffffff';
-//     generalInfosButton.style.backgroundColor    = '#ffffff';
-//     experiencesButton.style.backgroundColor     = '#ffffff';
-//     contractButton.style.backgroundColor        = '#ffffff';
-//     timeBankButton.style.backgroundColor        = '#f5f5f5';
-// });
+    managerViewGridButton.style.backgroundColor = '#ffffff';
+    generalInfosButton.style.backgroundColor    = '#ffffff';
+    experiencesButton.style.backgroundColor     = '#ffffff';
+    contractButton.style.backgroundColor        = '#ffffff';
+    timeBankButton.style.backgroundColor        = '#f5f5f5';
+});
 
 
 
